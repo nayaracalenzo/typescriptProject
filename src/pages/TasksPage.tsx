28a6af7dec392281dff1e131a737085e0ca1f44e
juliocesarfg2004 <juliocesarfg2004@gmail.com>
2026-01-29 11:57:11 -0300

    Filtrando os status das tarefas

--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -1,17 +1,42 @@
 import { useState } from "react"
 import TaskList from "../components/TaskList"
-import type { ITask } from "../type/type"
+import type { ITask, StatusFilter } from "../type/type"
 
 const TasksPage = () => {
-  const [tasks, setTasks] = useState<ITask[]>([{
-    id: 1,
-    title: "Estudar TypeScript",
-    priority: "high",
-    status: "todo"
-  }])
+  const [tasks] = useState<ITask[]>([
+    {
+      id: 1,
+      title: "Estudar TypeScript",
+      priority: "high",
+      status: "todo"
+    },
+    {
+      id: 2,
+      title: "Praticar Javascript",
+      priority: "medium",
+      status: "done"
+    }
+  ])
+
+  const [filter, setFilter] = useState<StatusFilter>("all")
+
+  const filterTasks = tasks.filter(task => {
+    if (filter === "all") return true
+    return task.status === filter
+  })
+
   return (
     <div>
-      <TaskList tasks={tasks}/>
+      <select
+        value={filter}
+        onChange={(e) => setFilter(e.target.value as StatusFilter)}
+      >
+        <option value="all">Todas</option>
+        <option value="todo">Todo</option>
+        <option value="doing">Doing</option>
+        <option value="done">Done</option>
+      </select>
+      <TaskList tasks={filterTasks} />
     </div>
   )
 }
